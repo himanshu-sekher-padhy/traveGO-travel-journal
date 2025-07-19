299e22a9475b8817c7790f897baafbc2c5b20759
Himanshu Sekher Padhy <166160381+himanshu-sekher-padhy@users.noreply.github.com>
2025-07-19 22:28:25 +0530

    Add files via upload

---
+++ src/touristPlaces.js
@@ -0,0 +1,76 @@
+import img1 from "../src/assets/places/img1.jpg";
+import img2 from "../src/assets/places/img2.jpg";
+import img3 from "../src/assets/places/img3.webp";
+import img4 from "../src/assets/places/img4.webp";
+import img5 from "../src/assets/places/img5.jpg";
+
+export default [
+    {
+        id: 1,
+        img: {
+            src: img1,
+            alt: "Mout Fuji"
+        },
+        googleMapsLink: "https://maps.app.goo.gl/bzeRJyErykd364s3A",
+        title: "Mount Fuji",
+        country: "JAPAN",
+        dates: "2 july, 2025 - 6 july, 2025",
+        text: "Mount Fuji, Japan’s highest mountain at 3,776 meters, is an iconic symbol of the country. A popular spot for hiking and photography, it's especially stunning during cherry blossom season."
+
+    },
+
+    {
+        id: 2,
+        img: {
+            src: img2,
+            alt: "Effiel Tower"
+        },
+        googleMapsLink: "https://maps.app.goo.gl/K6baDmZmsFF1xyH67",
+        title: "Effiel Tower",
+        country: "FRANCE",
+        dates: "6 july, 2025 - 11 july, 2025",
+        text: "The Eiffel Tower in Paris is one of the world’s most recognizable landmarks. Standing 300 meters tall, it offers panoramic views of the city. It was originally built for the 1889 World’s Fair."
+
+    },
+
+    {
+        id: 3,
+        img: {
+            src: img3,
+            alt: "Grand Canyon"
+        },
+        googleMapsLink: "https://maps.app.goo.gl/wrwWWkzRHQtQvY9n9",
+        title: "Grand Canyon",
+        country: "UNITED STATES",
+        dates: "9 july, 2025 - 12 july, 2025",
+        text: "The Grand Canyon in Arizona is a breathtaking natural wonder carved by the Colorado River over millions of years. Spanning 277 miles in length."
+    },
+
+    {
+        id: 4,
+        img: {
+            src: img4,
+            alt: "Neuschwanstein Castle"
+        },
+        googleMapsLink: "https://maps.app.goo.gl/yBPT8M7xWv2SaBTf6",
+        title: "Neuschwanstein Castle",
+        country: "GERMANY",
+        dates: "25 july, 2025 - 30 july, 2025",
+        text: "Nestled in the Bavarian Alps, Neuschwanstein Castle looks like it's straight out of a fairytale. It was built in the 19th century by King Ludwig II, it inspired Disney's Sleeping Beauty Castle."
+
+    },
+    {
+        id: 5,
+        img: {
+            src: img5,
+            alt: "Sheikh Zayed Grand Mosque"
+        },
+        googleMapsLink: "https://maps.app.goo.gl/MQ1mAxeJjT45dLkb9",
+        title: "Sheikh Zayed Grand Mosque",
+        country: "UAE",
+        dates: "1 August, 2025 - 10 August, 2025",
+        text: "Located in Abu Dhabi, the Sheikh Zayed Grand Mosque is one of the largest and most beautiful mosques in the world. It features 82 domes, 1000+ columns, and world’s largest hand-knotted carpet."
+
+    },
+
+]
